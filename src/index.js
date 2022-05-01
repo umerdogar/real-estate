a2405afd2007d0b646e4f0b3371d4af49b156425
Muhammad Umer <mumer@akru.co>
2022-05-01 16:11:20 +0500

    starting the project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.js';
+
+import $ from 'jquery';
+import Popper from 'popper.js';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
