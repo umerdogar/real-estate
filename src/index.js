a8428d74eb9a2cbdc04a768a836e3b4d554743ad
Muhammad Umer <mumer@akru.co>
2022-05-05 17:55:16 +0500

    redux code deleted

--- src/index.js
+++ src/index.js
@@ -9,24 +9,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import configureStore from "store";
-import { createStore, applyMiddleware } from 'redux'
 
-
-// const store = createStore(
-//   RootReducers, {},
-//    applyMiddleware(ReduxThunk));
-
-const store = createStore();
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
     ,
   </React.StrictMode>
 );
--- src/redux/actions/Property.js
+++
@@ -1,4 +0,0 @@
-export const getProperty = ( data ) => ({
-    type: 'GET_PROPERTY',
-    payload: data,
-})
