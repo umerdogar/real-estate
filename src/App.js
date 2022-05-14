4d1294c93247f9df922080e1ec98951d4f12b6b7
malik_Hamza <malik.hamza0546@gmail.com>
2022-05-14 16:19:37 +0500

    CodePushAfterThunkAPI

--- src/App.js
+++ src/App.js
@@ -4,21 +4,16 @@ import "./App.css"
 import Details from "./screen/Details"
 import { Provider } from "react-redux"
 import { applyMiddleware } from "redux"
-import { legacy_createStore as createStore , compose } from "redux"
+import { legacy_createStore as createStore, compose } from "redux"
 import ReduxThunk from "redux-thunk"
 import RootReducers from "./Redux/Reducers"
 import Routes from "../src/Routes"
 
-
-
-
-
 function App() {
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(RootReducers, composeEnhancers(applyMiddleware()))
+	// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+	// const store = createStore(RootReducers, composeEnhancers(applyMiddleware()))
 
-	
-	// const store = createStore(RootReducers, {}, applyMiddleware(ReduxThunk))
+	const store = createStore(RootReducers, {}, applyMiddleware(ReduxThunk))
 	return (
 		<Provider store={store}>
 			<div className="App">
--- src/Redux/Actions/actions.js
+++ src/Redux/Actions/actions.js
@@ -1,5 +1,4 @@
-import axios from "axios";
-
+import axios from "axios"
 
 export const loginUser = (response) => {
 	return {
@@ -8,15 +7,20 @@ export const loginUser = (response) => {
 	}
 }
 
-// export const loginUserTWo = () => {
-//     return dispatch => {
-//         console.log("GetUsers dispatch");
-//         axios.get(`localhost:8000/api/v1/property`)
-//         .then(res => {
-//             console.log("res" , res)
-//             const persons = res.data;
-//                 dispatch(loginUser(persons));
-    
-//         })
-//     };  
-// };
+export const loginUserTWo = () => {
+	// console.log("loginUserTWo")
+	return (dispatch) => {
+		console.log("GetUsers dispatch")
+		axios
+			.get(`http://52.220.87.52:8000/api/v1/property/popular/city`)
+			.then((res) => {
+				console.log("res")
+				console.log(res.data)
+				const persons = res.data
+				dispatch(loginUser(persons))
+			})
+			.catch((error) => {
+				console.log("error")
+			})
+	}
+}
