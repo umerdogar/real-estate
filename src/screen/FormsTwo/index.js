610d3b53813f90839302ba3f74ef2904abd8137c
malik_Hamza <malik.hamza0546@gmail.com>
2022-05-19 12:06:46 +0500

    tuu

--- src/screen/FormsTwo/index.js
+++ src/screen/FormsTwo/index.js
@@ -610,121 +610,121 @@ const FormsTwo = ({ allProperties, propertyDetail }) => {
 												<div className="col-md-8 col-12">
 													<div className="row">
 														{cardData &&
-															cardData.map((card) => (
-																<div className="col-md-4 margin-top-15 margin-bottom-15">
-																	<div className="card">
-																		<img
-																			className="card-img"
-																			src={card.image}
-																			alt="Vans"
-																		/>
-																		<div
-																			className="card-img-overlay  "
-																			style={{ padding: "0rem" }}
-																		>
-																			<a
-																				href="#"
-																				className="btn-small btn-danger mt-3"
-																				style={{ display: "inline" }}
-																			>
-																				{" "}
-																				{card?.propertyType?.featured}
-																			</a>
-																		</div>
-																		<div className=" d-flex ">
-																			<a
-																				href="#"
-																				className="btn-small btn-danger "
-																				style={{ marginTop: "-33px" }}
-																			>
-																				{" "}
-																				<span className="">
-																					{card?.rooms}
-																				</span>{" "}
-																				<i className="ri-camera-line"></i>
-																			</a>
-																		</div>
-																		<div className="card-img-overlay d-flex justify-content-end">
-																			<a
-																				href="#"
-																				className="card-link text-danger like"
-																			>
-																				<i className="ri-heart-line"></i>
-																			</a>
-																		</div>
-																		<div
-																			className="text-center"
-																			onClick={() => {
-																				// navigate("/details")
-																				console.log("Hamza")
-																			}}
-																		>
-																			<a
-																				href="#"
-																				className="btn-small btn-primary mt-3"
-																			>
-																				{" "}
-																				Detailss
-																			</a>
-																		</div>
-																		<div className="card-body">
-																			<h4 className="card-title">
-																				PKR 2.50 Crore
-																			</h4>
-																			<h6 className="card-subtitle mb-2 text-muted">
-																				I-8 Islamabad
-																			</h6>
-																			<ul className="list-inline margin-top-25">
-																				<li className="list-inline-item margin-right-5">
-																					<a>
-																						<i className="ri-hotel-bed-line"></i>
-																						<span className="margin-left-5 ">
-																							4
-																						</span>{" "}
-																					</a>
-																				</li>
-																				<li className="list-inline-item margin-right-5">
-																					<a>
-																						<i className="ri-heavy-showers-line"></i>{" "}
-																						<span className="margin-left-5 ">
-																							3
-																						</span>
-																					</a>
-																				</li>
-																				<li className="list-inline-item margin-right-5">
-																					<a>
-																						<i className="ri-fullscreen-fill"></i>
-																						<span className="margin-left-5 ">
-																							5987 sqft
-																						</span>
-																					</a>
-																				</li>
-																			</ul>
+															cardData.map((card) => {
+																return (
+																	<div className="col-md-4 margin-top-15 margin-bottom-15">
+																		<div className="card">
+																			<img
+																				className="card-img"
+																				src={card.image}
+																				alt="Vans"
+																			/>
 																			<div
-																				className="buy d-flex  align-items-center"
+																				className="card-img-overlay  "
 																				style={{
-																					justifyContent: "space-evenly",
+																					padding: "0rem",
 																				}}
 																			>
 																				<a
 																					href="#"
-																					className="btn-small btn-primary mt-3"
+																					className="btn-small btn-danger mt-3"
+																					style={{
+																						display: "inline",
+																					}}
 																				>
 																					{" "}
-																					Call
+																					{card?.propertyType?.featured}
 																				</a>
+																			</div>
+																			<div className=" d-flex ">
 																				<a
 																					href="#"
-																					className="btn-small btn-primary mt-3"
+																					className="btn-small btn-danger "
+																					style={{
+																						marginTop: "-33px",
+																					}}
 																				>
 																					{" "}
-																					Email
+																					<span className="">
+																						{card?.rooms}
+																					</span>{" "}
+																					<i className="ri-camera-line"></i>
 																				</a>
+																			</div>
+																			<div className="card-img-overlay d-flex justify-content-end">
+																				<a
+																					href="#"
+																					className="card-link text-danger like"
+																				>
+																					<i className="ri-heart-line"></i>
+																				</a>
+																			</div>
+																			<div
+																				onClick={(e) => {
+																					console.log("Hamza")
+																				}}
+																			>
+																				<p> Detailss</p>
+																			</div>
+																			<div className="card-body">
+																				<h4 className="card-title">
+																					PKR 2.50 Crore
+																				</h4>
+																				<h6 className="card-subtitle mb-2 text-muted">
+																					I-8 Islamabad
+																				</h6>
+																				<ul className="list-inline margin-top-25">
+																					<li className="list-inline-item margin-right-5">
+																						<a>
+																							<i className="ri-hotel-bed-line"></i>
+																							<span className="margin-left-5 ">
+																								4
+																							</span>{" "}
+																						</a>
+																					</li>
+																					<li className="list-inline-item margin-right-5">
+																						<a>
+																							<i className="ri-heavy-showers-line"></i>{" "}
+																							<span className="margin-left-5 ">
+																								3
+																							</span>
+																						</a>
+																					</li>
+																					<li className="list-inline-item margin-right-5">
+																						<a>
+																							<i className="ri-fullscreen-fill"></i>
+																							<span className="margin-left-5 ">
+																								5987 sqft
+																							</span>
+																						</a>
+																					</li>
+																				</ul>
+																				<div
+																					className="buy d-flex  align-items-center"
+																					style={{
+																						justifyContent: "space-evenly",
+																					}}
+																				>
+																					<a
+																						href="#"
+																						className="btn-small btn-primary mt-3"
+																					>
+																						{" "}
+																						Call
+																					</a>
+																					<a
+																						href="#"
+																						className="btn-small btn-primary mt-3"
+																					>
+																						{" "}
+																						Email
+																					</a>
+																				</div>
 																			</div>
 																		</div>
 																	</div>
-																</div>
-															))}
+																)
+															})}
 
 														{/* <div className="col-md-4 margin-top-15 margin-bottom-15">
 
